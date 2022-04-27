823ac816a1c9444c8707e4eb46af3f812b0e9a35
Azeem <59115414+azeemade@users.noreply.github.com>
2022-04-27 20:28:14 +0100

    first commit + update to json data to seed user table

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('role');
+            $table->string('created_at');
+            $table->string('role_type');
+            $table->string('password');
+            $table->string('mobile');
+            $table->string('username');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        $this->userSeeder();
+    }
+
+    public function userSeeder()
+    {
+        $faker = \Faker\Factory::create();
+
+        $userFile = Storage::disk('public')->get("seed/user.json");
+
+        $user = json_decode($userFile);
+        foreach ($user as $key => $value) {
+            User::create([
+                "firstname" => $value->firstname,
+                "lastname" => $value->lastname,
+                "email" => $value->email,
+                "role" => $value->role,
+                "created_at" => $value->created_at,
+                "role_type" => $value->role_type,
+                "password" => Hash::make($faker->password(8, 15)),
+                "mobile" => $faker->phoneNumber(),
+                "username" => $faker->userName(),
+            ]);
+        }
+    }
+}
